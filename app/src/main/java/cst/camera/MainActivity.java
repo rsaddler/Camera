39213924588d6d315217c8e96c5bbf72e6541882
rsaddler <radcliffesaddler@students.ptechnyc.org>
2015-05-12 21:14:35 -0400

    Camera Part to the add
    This part just needs to be added with the facebook part.
    I couldnt get the formFile thing to work not sure what is.

---
+++ app/src/main/java/cst/camera/MainActivity.java
@@ -0,0 +1,51 @@
+package cst.camera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    private File imageFile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//This part below saves the image
+    public void process(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageFile=new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"test.jpg");
+       Uri FirstU=Uri.formFile(imageFile);//Here is my problem I dont know how to get ride of it.
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,FirstU);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, 0);
+    }
+    @Override//checks where to save the image.
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        if(requestCode==0)
+        {
+            switch (resultCode){// A check to see if the image file exists
+                case Activity.RESULT_OK:
+                    if(imageFile.exists())
+                    {
+                        Toast.makeText(this,"The file was saved at "+imageFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                    }
+                    else
+                {
+                    Toast.makeText(this,"No Save Data", Toast.LENGTH_LONG).show();//Just incase there is no save
+                }
+                    break;
+                case Activity.RESULT_CANCELED:    // this is used just incase the user doesnt want the picture
+                 break;
+                default:
+                    break;
+            }
+        }
+    }
+}
